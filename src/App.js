227d7dc7966fe4bd6905d9d34ccc5adcea46ad65
Umair Shahid <umair.shahid0088@gmail.com>
2020-10-22 00:56:48 +0500

    Covid19 Tracker second design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Chart} from './Component/Charts/Chart';
+import {Cards} from './Component/Cards/Cards';
+import {CountryPicker} from './Component/CountryPicker/CountryPicker';
+import Styles from './App.module.css';
+import {fetchData} from './api';
+import coronaImage from './images/image.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+
+state={
+  data:{},
+  country:'',
+}
+  async componentDidMount(){
+    const fetchedData=await fetchData();
+  this.setState({data:fetchedData});  
+  }
+
+  handleCountryChange= async (country)=>{
+    const fetchedData=await fetchData(country);
+    this.setState({data:fetchedData, country: country});  
+  
+  }
+
+  render(){
+const {data, country} =this.state;
+
+return(
+          <div className={Styles.container}>
+            <img src={coronaImage} className={Styles.image} alt="COVID-19"/>
+          <Cards data={data}/>
+          <CountryPicker handleCountryChange={this.handleCountryChange}/>
+          <Chart data={data} country={country} />
+         
+           </div>
+
+    )
+  }
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <div className={Styles.container}>
+//    <Cards/>
+//    <CountryPicker/>
+//    <Chart/>
+   
+//     </div>
+//   );
+// }
+
---
+++ src/Component/Cards/Cards.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import Styles from './Cards.module.css';
+import CountUp from 'react-countup';
+import cx from 'classnames';
+
+export const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
+    if(!confirmed){
+       return "Loading...";
+   }
+
+    return (
+        <div className={Styles.container}>
+           <Grid container spacing={3} justify="center">
+               <grid item component={Card} xs={12} md={3} className={cx(Styles.card, Styles.infected)}>
+                   <CardContent>
+                       <Typography color="textSecondary" gutterBottom>
+                          Infected
+                       </Typography>
+                       
+                        <Typography variant="h5">
+                            
+                       <CountUp start={0} end={confirmed.value} duration={2.5} separator=","/>
+                        </Typography>
+
+                        <Typography color="textSecondary">
+                        {new Date(lastUpdate).toDateString()}
+                        </Typography>
+
+                        <Typography variant="body2">
+                            Number of active cases of COVID-19
+                        </Typography>
+                   </CardContent>
+
+               </grid>
+
+
+               <grid item component={Card} xs={12} md={3} className={cx(Styles.card, Styles.recovered)}>
+                   <CardContent>
+                       <Typography color="textSecondary" gutterBottom>
+                          Recovered
+                       </Typography>
+                       
+                        <Typography variant="h5">
+                            
+                            <CountUp start={0} end={recovered.value} duration={2.5} separator=","/>
+                     
+                        </Typography>
+
+                        <Typography color="textSecondary">
+                        {new Date(lastUpdate).toDateString()}
+                        </Typography>
+
+                        <Typography variant="body2">
+                            Number of recoveries COVID-19
+                        </Typography>
+                   </CardContent>
+
+               </grid>
+
+
+               <grid item component={Card} xs={12} md={3} className={cx(Styles.card, Styles.deaths)}>
+                   <CardContent>
+                       <Typography color="textSecondary" gutterBottom>
+                          Deaths
+                       </Typography>
+                       
+                        <Typography variant="h5">
+                            
+                            <CountUp start={0} end={deaths.value} duration={2.5} separator=","/>
+                     
+                        </Typography>
+
+                        <Typography color="textSecondary">
+                            {new Date(lastUpdate).toDateString()}
+                     
+                        </Typography>
+
+                        <Typography variant="body2">
+                            Number of deaths caused by COVID-19
+                        </Typography>
+                   </CardContent>
+
+               </grid>
+               
+               </Grid> 
+        </div>
+    )
+}
