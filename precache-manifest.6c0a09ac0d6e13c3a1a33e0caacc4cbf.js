cd7e89c70a33af4b97fcfd76085f845cf589c6d8
Umair Shahid <umair.shahid0088@gmail.com>
2021-01-14 20:34:39 +0500

    Updates

---
+++ precache-manifest.6c0a09ac0d6e13c3a1a33e0caacc4cbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad7c5a28c8926980d3d497179a85e72",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/index.html"
+  },
+  {
+    "revision": "0c05ad55ebb1b5feb69e",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/css/main.c5800b26.chunk.css"
+  },
+  {
+    "revision": "b1aa3d67e2bc7445a118",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/js/2.673fae2d.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/js/2.673fae2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c05ad55ebb1b5feb69e",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/js/main.8dbe35bb.chunk.js"
+  },
+  {
+    "revision": "649b4e40f2b031f10a3b",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/js/runtime-main.e743eb01.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Covid19-Tracker-in-reactjs-Second-Design/static/media/logo.5d5d9eef.svg"
+  }
+]);
